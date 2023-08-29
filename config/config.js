7bb13e29550ada960e4c44f98ec5ea89da7be1fe
iammradarsh <gachhaadarsh122@gmail.com>
2023-08-29 09:11:47 +0530

    Social Media

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "blogsessionuserssecret";
+
+const emailUser = "adarshgachha@gmail.com";
+const emailPassword = "1234567890123456789";
+
+module.exports = {
+    sessionSecret,
+    emailUser,
+    emailPassword
+}
