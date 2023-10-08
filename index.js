bc5ce30ff94fa3c749c739d8ffe637418f7b53c2
iammradarsh <gachhaadarsh122@gmail.com>
2023-10-08 08:15:40 +0530

    mongo url change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://0.0.0.0:27017/BMS");
+// mongoose.connect("mongodb://0.0.0.0:27017/BMS");
+mongoose.connect("mongodb://0.0.0.0:27017/blog");
 
 
 const express = require("express");
